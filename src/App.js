e795506c0d377894362bc3449e10ddbf857e1713
Rodrigo Waltmann Nascimento <rodrigowaltmann.n@gmail.com>
2022-10-29 17:44:19 -0300

    Aplicação de estilo

--- src/App.js
+++ src/App.js
@@ -6,28 +6,28 @@ function App() {
     <div className="container">
       <h1 className="title">Buscador de cep</h1>
 
-    <div className="containerInput">
-      <input
-      type="text"
-      placeholder="Digite seu cep..."
-      />
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep..."
+        />
 
-    <button className="buttonSearch">
-      <FiSearch size={25} color="#fff"/>
-    </button>
-    
-    </div>
+      <button className="buttonSearch">
+        <FiSearch size={25} color="#fff"/>
+      </button>
+      
+      </div>
 
-    <main>
-      <h2>CEP: 40028922</h2>
+      <main className='main'>
+        <h2>CEP: 40028922</h2>
 
-      <span>Rua: Alguma rua</span>
-      <span>Complemento: Algum Complemento</span>
-      <span>Complemento: Algum Complemento</span>
-      <span>Teste</span>
-      <span>Algo aqui também</span>
+        <span>Rua: Alguma rua</span>
+        <span>Complemento: Algum Complemento</span>
+        <span>Complemento: Algum Complemento</span>
+        <span>Teste</span>
+        <span>Algo aqui também</span>
 
-    </main>
+      </main>
     </div>
   );
 }
