ad35a707d44867c76a5eee8dd315ad8a4b001fbc
DDricko <rodrigowaltmann.n@gmail.com>
2022-10-29 21:08:52 -0300

    Configurações para consumo da api  do viacep

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi'
 import './styles.css'
 
+import api from './services/api'
+
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch(){
+    
+    if(input === ''){
+      alert("Favor informar um cep")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+
+    }catch{
+      alert("Erro ao buscar cep");
+      setInput("");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador de cep</h1>
@@ -10,24 +35,27 @@ function App() {
         <input
         type="text"
         placeholder="Digite seu cep..."
+        value={input}
+        onChange={(e) => setInput(e.target.value) }
         />
 
-      <button className="buttonSearch">
+      <button className="buttonSearch" onClick={handleSearch}>
         <FiSearch size={25} color="#fff"/>
       </button>
       
       </div>
 
-      <main className='main'>
-        <h2>CEP: 40028922</h2>
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
 
-        <span>Rua: Alguma rua</span>
-        <span>Complemento: Algum Complemento</span>
-        <span>Complemento: Algum Complemento</span>
-        <span>Teste</span>
-        <span>Algo aqui também</span>
-
-      </main>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+          
+        </main>
+      )}
     </div>
   );
 }
